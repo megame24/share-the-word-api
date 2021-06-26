76254ac7e4ecc366d2b7a63c09a9642791c84f3a
Innocent Ngene <ngeneinnocent@gmail.com>
2021-06-26 20:01:39 +0100

    update error service

--- src/modules/shared/infrastructure/services/errorService.ts
+++ src/modules/shared/infrastructure/services/errorService.ts
@@ -1,20 +1,34 @@
 class CustomError extends Error {
-  status: number;
+  statusCode: number;
 
-  constructor(message: string, status: number) {
+  constructor(message: string, statusCode: number) {
     super(message);
-    this.status = status;
+    this.statusCode = statusCode;
   }
 }
 
 export interface ErrorService {
-  throwError: (message: string, status: number) => CustomError;
+  badRequestError: (message: string) => CustomError;
+  unauthorizedError: (message: string) => CustomError;
+  forbiddenError: (message: string) => CustomError;
+  internalServerError: (message: string) => CustomError;
 }
 
 export class ErrorServiceImplementation implements ErrorService {
-  throwError(message: string, status = 500): CustomError {
-    const err = new CustomError(message, status);
-    throw err;
+  badRequestError(message: string): CustomError {
+    return new CustomError(message, 400);
+  }
+
+  unauthorizedError(message: string): CustomError {
+    return new CustomError(message, 401);
+  }
+
+  forbiddenError(message: string): CustomError {
+    return new CustomError(message, 403);
+  }
+
+  internalServerError(message: string): CustomError {
+    return new CustomError(message, 500);
   }
 
   // put error middleware here.
