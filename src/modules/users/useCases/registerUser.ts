472354c500ec22ed322f6cfdb5d8bd6c44e1cd8a
Innocent Ngene <ngeneinnocent@gmail.com>
2021-06-27 23:16:45 +0100

    register user use case update

--- src/modules/users/useCases/registerUser.ts
+++ src/modules/users/useCases/registerUser.ts
@@ -1,6 +1,30 @@
+import User from "../entities/user";
+import { SecurityService } from "../infrastructure/services/securityService";
+import { UUIDService } from "../infrastructure/services/uuidService";
+
+interface RegisterUserDTO {
+  username: string;
+  email: string;
+  password: string;
+  name: string;
+}
+
 export default class registerUser {
   // have a public execute function
   // the controller will pass a registerUserDTO
   // check for already existing email and username
   // create user
+  // and save it
+  // return nothing
+  constructor(
+    private securityService: SecurityService,
+    private uuidService: UUIDService
+  ) {}
+
+  execute(registerUserDTO: RegisterUserDTO): Promise<User> {
+    // the repositories should handle errors that can arise from interacting
+    // with external db packages
+
+    return User.create(registerUserDTO, this.securityService, this.uuidService);
+  }
 }
